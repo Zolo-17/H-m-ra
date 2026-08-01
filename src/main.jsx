3feafbed7022435e8d6b915a9f3f891643f295d0
Zolo-17 <zolo.theophile@gmail.com>
2026-08-01 18:11:59 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import PaymentManual from "./PaymentManual.jsx";
+import AdminApprovals from "./AdminApprovals.jsx";
+
+function Root() {
+  const path = window.location.pathname;
+  if (path.startsWith("/paiement")) return <PaymentManual />;
+  if (path.startsWith("/admin")) return <AdminApprovals />;
+  return <App />;
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>
 );
+
