503f3503e49c927705299dbec7d74599fffbc621
Zolo-17 <zolo.theophile@gmail.com>
2026-08-05 12:58:37 +0100

    Refactor main.jsx to use BrowserRouter
    Refactored the rendering logic to use BrowserRouter and removed the Root component.

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import PaymentManual from "./PaymentManual.jsx";
-import AdminApprovals from "./AdminApprovals.jsx";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
 
-function Root() {
-  const path = window.location.pathname;
-  if (path.startsWith("/paiement")) return <PaymentManual />;
-  if (path.startsWith("/admin")) return <AdminApprovals />;
-  return <App />;
-}
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Root />
-  </React.StrictMode>
-);
-
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+)
