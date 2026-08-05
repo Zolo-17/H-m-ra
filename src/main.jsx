d601cc50313f3219858d102dbcbb2aa9ad80c4d8
Zolo-17 <zolo.theophile@gmail.com>
2026-08-05 13:19:35 +0100

    Update main.jsx to include route-based rendering
    Refactor main component to conditionally render based on the path.

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import PaymentManual from "./PaymentManual.jsx";
+import AdminApprovals from "./AdminApprovals.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+function Root() {
+  const path = window.location.pathname;
+  if (path.startsWith("/paiement")) return <PaymentManual />;
+  if (path.startsWith("/admin")) return <AdminApprovals />;
+  return <App />;
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+    <Root />
+  </React.StrictMode>
+);
+
